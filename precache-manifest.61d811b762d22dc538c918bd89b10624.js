a693cda36304008416bcad8fab116cb16562e9e3
iprowriter <46689083+iprowriter@users.noreply.github.com>
2020-03-31 10:40:00 +0200

    Updates

---
+++ precache-manifest.61d811b762d22dc538c918bd89b10624.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151c58c58eaaa289c6d9db6aa8e8a6c1",
+    "url": "/weathercheck/index.html"
+  },
+  {
+    "revision": "bcc27a19b6d5eee48457",
+    "url": "/weathercheck/static/css/main.7839f605.chunk.css"
+  },
+  {
+    "revision": "c24b1b304a4bc9624974",
+    "url": "/weathercheck/static/js/2.28885a1b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weathercheck/static/js/2.28885a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc27a19b6d5eee48457",
+    "url": "/weathercheck/static/js/main.82f3a31a.chunk.js"
+  },
+  {
+    "revision": "7dc0add0696461d604da",
+    "url": "/weathercheck/static/js/runtime-main.0c50c79e.js"
+  }
+]);
